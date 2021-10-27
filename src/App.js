51ba0d52711fd00857c6bfedf9538a84c7d6a0b6
David Wampamba <davidwampamba@gmail.com>
2021-10-27 18:06:03 +0300

    App done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function App() {
+
+    const [theme, switchTheme] = React.useState('black')
+
+    const handleClick = (switchTo) => {
+        switchTheme(switchTo)
+    }
+
+    return (
+        <div className={`container ${theme}`}>
+            <h1>The theme is <SayTheme themeName={theme} /></h1>
+
+            <div>
+                <input type="button" className="btn black" onClick={() => handleClick('black')}/>
+            <input type="button" className="btn blue"  onClick={() => handleClick('blue')}/>
+            <input type="button" className="btn yellow" onClick={() => handleClick('yellow')}/>
+            </div>
+        </div>
+    )
+}
+
+export default App
+
+function SayTheme (props) {
+    return <span>{props.themeName}</span>
+}
